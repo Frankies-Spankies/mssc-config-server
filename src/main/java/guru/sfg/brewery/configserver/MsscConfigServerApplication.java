26f73308d0967428b6cc24593f31a9ddbc30310d
franciscogamezdg <franciscogamezdg@gmail.com>
2020-10-26 17:18:03 -0600

    Agrega dependencias y archivos de loging: * zipkin(sluth), logstash(JSON) * archivos logback-spring.xml para configuracion de logs con json

--- src/main/java/guru/sfg/brewery/configserver/MsscConfigServerApplication.java
+++ src/main/java/guru/sfg/brewery/configserver/MsscConfigServerApplication.java
@@ -2,7 +2,9 @@ package guru.sfg.brewery.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer //Configuracion automatica para spring cloud config
 @SpringBootApplication
 public class MsscConfigServerApplication {
 
